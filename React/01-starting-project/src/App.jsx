1d38fcf2377489931b93f030ced860c64eedf16e
siddiqueeminasu <71393717+siddiqueeminasu@users.noreply.github.com>
2025-09-21 18:02:24 +0600

    Using & outputting dynamic values

---
+++ React/01-starting-project/src/App.jsx
@@ -0,0 +1,33 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"]
+
+function genRendomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() { // Header component definition
+  const description = reactDescriptions[genRendomInt(2)];
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;

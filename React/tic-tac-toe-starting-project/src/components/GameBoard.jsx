90ef6d9fed6586898698cca37c3701337b577482
rabidjahankhan <rabidjahankhan@gmail.com>
2025-09-25 11:27:42 +0600

    updating object state immutably

--- React/tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ React/tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +8,24 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquer(rowIndex, colIndex ) {
+        setGameBoard((prevGameBoard) => {
+          const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+          updatedBoard[rowIndex][colIndex] = "X";
+          return updatedBoard;
+        });
+    }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymble, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymble}</button>
+                <button onClick={() => handleSelectSquer(rowIndex, colIndex)}>{playerSymble}</button>
               </li>
             ))}
           </ol>

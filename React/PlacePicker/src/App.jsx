4fbd7a838b5756c74ae617d99b442dbdb24acf86
rabidjahankhan <rabidjahankhan@gmail.com>
2025-10-22 14:32:12 +0600

    Optimizing State

--- React/PlacePicker/src/App.jsx
+++ React/PlacePicker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -55,7 +55,7 @@ function App() {
     localStorage.setItem("selectedPlaces", JSON.stringify([id, ...storedIds]));
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -65,15 +65,17 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
       <Modal open={modalIsOpen}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
+        {modalIsOpen && (
+          <DeleteConfirmation
+            onCancel={handleStopRemovePlace}
+            onConfirm={handleRemovePlace}
+          />
+        )}
       </Modal>
 
       <header>
--- React/PlacePicker/src/components/Modal.jsx
+++ React/PlacePicker/src/components/Modal.jsx
@@ -1,11 +1,20 @@
-import {forwardRef, useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 function Modal({open, children }) {
   const dialog = useRef();
 
+  useEffect(() => {
+    if(open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open])
+
+
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
     document.getElementById('modal')

c18a480c08cc43ee6dd2a7a53a1588b0d315d42d
rabidjahankhan <rabidjahankhan@gmail.com>
2025-11-03 16:25:47 +0600

    fetching meals data via http request

--- React/Food-order-app/src/App.jsx
+++ React/Food-order-app/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./assets/components/Header.jsx";
+import Meals from "./assets/components/Meals.jsx";
+
 function  App() {
   return (
     <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <Header />
+      <Meals/>
     </>
   );
 }

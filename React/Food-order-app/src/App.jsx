20d889d00eecdf4c14aec7e980bbc1af5ddf977a
rabidjahankhan <rabidjahankhan@gmail.com>
2025-11-03 19:42:56 +0600

    Finishing & Using the Cart Context & Reducer in progress

--- React/Food-order-app/src/App.jsx
+++ React/Food-order-app/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./assets/components/Header.jsx";
 import Meals from "./assets/components/Meals.jsx";
+import { CartContextProvider } from "./store/CartContext.jsx";
 
 function  App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals/>
-    </>
+    </CartContextProvider>
   );
 }
 
--- React/Food-order-app/src/assets/components/MealItem.jsx
+++ React/Food-order-app/src/assets/components/MealItem.jsx
@@ -1,15 +1,26 @@
+import { useContext } from 'react';
+import Button from './UI/Button.jsx';
+import { currencyFormatter } from "../../util/formatting.js  ";
+import { useContext } from 'react';
+import CartContext from '../../store/CartContext.jsx';
+
 export default function MealItem({ meal }) {
+    const cartCtx = useContext(CartContext);
+
+    function handleAddMealToCart() {
+        cartCtx.addItem(meal);
+    };
   return (
     <li className="meal-item">
       <article>
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
             <h3>{meal.name}</h3>
-            <p className="meal-item-price">{meal.price}</p>
+            <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
             <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-            <button>Add to Cart</button>
+            <Button onClick={handleAddMealToCart} >Add to Cart</Button>
         </p>
       </article>
     </li>

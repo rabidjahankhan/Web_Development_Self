3b74f0c8e457c3fff58c64f3698cf8a90cac717c
rabidjahankhan <rabidjahankhan@gmail.com>
2025-11-07 19:54:38 +0600

    finished

--- React/Redux-Project/src/store/index.js
+++ React/Redux-Project/src/store/index.js
@@ -1,11 +1,11 @@
 //import { legacy_createStore as createStore, combineReducers } from "redux";
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
+  name: "counter",
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -18,14 +18,31 @@ const counterSlice = createSlice({
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
-    }
-  }
+    },
+  },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authintication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;

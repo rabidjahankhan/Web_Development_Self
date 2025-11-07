92cc22b0703c0976b1a3d26bfa160c93580e389a
rabidjahankhan <rabidjahankhan@gmail.com>
2025-11-07 18:40:28 +0600

    redux toolkit

--- React/Redux-Project/src/App.js
+++ React/Redux-Project/src/App.js
@@ -1,9 +1,16 @@
+import { Fragment } from 'react';
+import Header from './components/Header';
 import Counter from './components/Counter';
+import Auth from './components/Auth';
 
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header/>
+      <Auth />
+      <Counter />
+    </Fragment>
   );
 }
 

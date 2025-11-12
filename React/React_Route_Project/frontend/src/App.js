d93e5bc7fbeba9fc356b0e12039951bfd9bb3b9d
rabidjahankhan <rabidjahankhan@gmail.com>
2025-11-12 12:43:31 +0600

    Output a list of dummy events to the EventsPage done

--- React/React_Route_Project/frontend/src/App.js
+++ React/React_Route_Project/frontend/src/App.js
@@ -13,11 +13,16 @@
 //    - /events/<some-id> => EventDetailPage
 //    - /events/new => NewEventPage
 //    - /events/<some-id>/edit => EditEventPage
+//DONE
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
+//DONE
 // 4. Add properly working links to the MainNavigation
+//DONE
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
+//DONE
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
+//DONE
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -34,11 +39,11 @@ const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/events", element: <EventsPage /> },
-      { path: "/events/:eventId", element: <EventDetailPage /> },
-      { path: "/events/new", element: <NewEventPage /> },
-      { path: "/events/:eventId/edit", element: <EditEventPage /> },
+      { index: true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
     ],
   },
 ]);
--- React/React_Route_Project/frontend/src/pages/Events.js
+++ React/React_Route_Project/frontend/src/pages/Events.js
@@ -1,5 +1,41 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "React Beginners Workshop",
+  },
+  {
+    id: "e2",
+    title: "JavaScript Coding Challenge",
+  },
+  {
+    id: "e3",
+    title: "Fullstack Developer Meetup",
+  },
+  {
+    id: "e4",
+    title: "Frontend UI/UX Design Conference",
+  },
+  {
+    id: "e5",
+    title: "Node.js Backend Bootcamp",
+  },
+];
+
 function EventsPage() {
-    return <h1>EventsPage</h1>
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;

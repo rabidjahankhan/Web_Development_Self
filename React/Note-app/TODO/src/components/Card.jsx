081296bee11462dc97ad23fe05959650ad508a00
rabidjahankhan <rabidjahankhan@gmail.com>
2025-11-25 16:00:05 +0600

    fixing

--- React/Note-app/TODO/src/components/Card.jsx
+++ React/Note-app/TODO/src/components/Card.jsx
@@ -3,9 +3,11 @@ import "./Card.css";
 
 export default function Card({title, content}) {
     return (
-        <div className="card">
-            <h2>{title}</h2>
-            <p>{content}</p>
+        <div className="card-container">
+        <div className="card-item">
+            <h2>Title</h2>
+            <p>Content</p>
+        </div>
         </div>
     );
 }

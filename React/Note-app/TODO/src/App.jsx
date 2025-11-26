8c5063e8ec24912f1862a79a800463665c3c5ba6
Rabid-Jahan-Khan <rabidjahankhan@gmail.com>
2025-11-27 01:54:46 +0600

    fixing bug

--- React/Note-app/TODO/src/App.jsx
+++ React/Note-app/TODO/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NoteInput from "./components/NoteInput";
 import Card from "./components/Card";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
+  // function addNote(newNote) {
+  //   setNotes((prev) => [...prev, newNote]);
+  // }
+
+  //load
+  useEffect(() => {
+    const storedNotes = JSON.parse(localStorage.getItem("notes"));
+    if (storedNotes) {
+      setNotes(storedNotes);
+    }
+  }, []);
+
+  //save
+   useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
   function addNote(newNote) {
     setNotes((prev) => [...prev, newNote]);
   }
+
 
   return (
     <>

75f68a2d30e7d29a20e194466a888bbeaed7ce57
rabidjahankhan <rabidjahankhan@gmail.com>
2025-11-27 17:49:28 +0600

    editing code for background option

--- React/Note-app/TODO/src/components/Card.jsx
+++ React/Note-app/TODO/src/components/Card.jsx
@@ -1,10 +1,10 @@
 import "./Card.css";
 
 
-export default function Card({title, content}) {
+export default function Card({title, content, backgroundColor}) {
     return (
         <div className="card-container">
-        <div className="card-item">
+        <div className="card-item" style={{backgroundColor}}>
             <strong><h2>{title}</h2></strong>
             <p>{content}</p>
         </div>

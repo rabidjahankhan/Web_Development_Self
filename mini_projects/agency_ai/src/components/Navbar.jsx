ee2e81f0adbb612e3c6696171d7d1f2dbdc4a937
rabidjahankhan <159470843+rabidjahankhan@users.noreply.github.com>
2026-07-15 19:59:07 +0600

    Navbar theme added

--- mini_projects/agency_ai/src/components/Navbar.jsx
+++ mini_projects/agency_ai/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import assets from "../assets/assets";
+import  {ThemeToggleBtn}  from "./ThemeToggleBtn";
 
 const Navbar = ({ theme, setTheme }) => {
+
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -28,21 +30,40 @@ const Navbar = ({ theme, setTheme }) => {
           onClick={() => setSidebarOpen(false)}
         />
 
-        <a href="#" className="sm:hover:border-b">
+        <a
+          onClick={() => setSidebarOpen(false)}
+          href="#"
+          className="sm:hover:border-b"
+        >
           Home
         </a>
-        <a href="#services" className="sm:hover:border-b">
+        <a
+          onClick={() => setSidebarOpen(false)}
+          href="#services"
+          className="sm:hover:border-b"
+        >
           Services
         </a>
-        <a href="#our-work" className="sm:hover:border-b">
+        <a
+          onClick={() => setSidebarOpen(false)}
+          href="#our-work"
+          className="sm:hover:border-b"
+        >
           Our Work
         </a>
-        <a href="#contact-us" className="sm:hover:border-b">
+        <a
+          onClick={() => setSidebarOpen(false)}
+          href="#contact-us"
+          className="sm:hover:border-b"
+        >
           Contact Us
         </a>
       </div>
 
       <div className="flex items-center gap-2 sm:gap-4">
+
+        <ThemeToggleBtn theme={theme} setTheme={setTheme} />
+
         <img
           src={theme === "dark" ? assets.menu_icon_dark : assets.menu_icon}
           alt=""
